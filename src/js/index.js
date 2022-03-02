8e13bbc9f14edbf29235598353d81efb4a9104df
angelique-df <faye.angelique.d@gmail.com>
2022-03-02 23:44:16 +0000

    Added JavaScript code
    - Fetches and displays a random advice when the dice button is clicked.

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const findNewAdvice = async () => {
+  const response = await fetch('https://api.adviceslip.com/advice')
+  const advice = await response.json()
+
+  return advice
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const newIdButton = document.getElementById('new-advice')
+
+  newIdButton.addEventListener('click', async () => {
+    const adviceID = document.getElementById('advice-id')
+    const adviceText = document.getElementById('advice-text')
+
+    const { slip: { id, advice } } = await findNewAdvice()
+
+    adviceID.textContent = id
+    adviceText.textContent = advice
+  })
+})
