68db6dbfb8d7b16d40a5791ce3bc6f4733ec0b76
angelique-df <faye.angelique.d@gmail.com>
2022-07-20 18:17:58 +0000

    `prettier` formatting

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,12 @@ const findNewAdvice = async () => {
 
 const newAdviceBtn = document.getElementById("new-advice");
 
-  newAdviceBtn.addEventListener('click', async () => {
-    const adviceID = document.getElementById('advice-id')
-    const adviceText = document.getElementById('advice-text')
+newAdviceBtn.addEventListener("click", async () => {
+	const adviceID = document.getElementById("advice-id");
+	const adviceText = document.getElementById("advice-text");
 
     const { slip: { id, advice } } = await findNewAdvice()
 
     adviceID.textContent = id
     adviceText.textContent = advice
-  })
+});
