ca616bd8efba5162f9a165c1f11a254017395075
angelique-df <faye.angelique.d@gmail.com>
2022-07-20 18:21:26 +0000

    Remove unneeded event listener

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,19 @@
-window.addEventListener("DOMContentLoaded", async () => {
-	const newAdviceBtn = document.getElementById("new-advice");
+const newAdviceBtn = document.getElementById("new-advice");
 
-	newAdviceBtn.addEventListener("click", () => {
-		const adviceID = document.getElementById("advice-id");
-		const adviceText = document.getElementById("advice-text");
+newAdviceBtn.addEventListener("click", () => {
+	const adviceID = document.getElementById("advice-id");
+	const adviceText = document.getElementById("advice-text");
 
-		fetch("https://api.adviceslip.com/advice")
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				const {
-					slip: { id, advice },
-				} = data;
+	fetch("https://api.adviceslip.com/advice")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+			const {
+				slip: { id, advice },
+			} = data;
 
-				adviceID.textContent = id;
-				adviceText.textContent = advice;
-			})
-			.catch((error) => console.log(error));
-	});
+			adviceID.textContent = id;
+			adviceText.textContent = advice;
+		})
+		.catch((error) => console.log(error));
 });
